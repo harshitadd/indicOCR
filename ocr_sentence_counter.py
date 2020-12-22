75ba7b3cc37f71d571c73429a64ec5351ef2183a
Harshitadd <31439716+harshitadd@users.noreply.github.com>
2020-12-22 17:08:07 +0530

    Update ocr_sentence_counter.py

--- ocr_sentence_counter.py
+++ ocr_sentence_counter.py
@@ -19,14 +19,6 @@ image_counter = 1
   
 # Iterate through all the pages stored above 
 for page in pages: 
-  
-    # Declaring filename for each page of PDF as JPG 
-    # For each page, filename will be: 
-    # PDF page 1 -> page_1.jpg 
-    # PDF page 2 -> page_2.jpg 
-    # PDF page 3 -> page_3.jpg 
-    # .... 
-    # PDF page n -> page_n.jpg 
     filename = "page_"+str(image_counter)+".jpg"
       
     # Save the image of the page in system 
