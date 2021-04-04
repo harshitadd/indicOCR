51ff1e9d6d4abfbceb37c4799b4b8292f2e57d61
Harshitadd <31439716+harshitadd@users.noreply.github.com>
2021-04-05 00:32:52 +0530

    Update concatenate_files.py

--- concatenate_files.py
+++ concatenate_files.py
@@ -1,11 +1,29 @@
-import os 
-import tarfile
-input_path = '/home/harshitadd/gu_darts/'
-years = os.listdir(input_path)
-for year in years:
-  year_dir = tarfile.open(year)
-  year_dir.extractall(input_path)
-  year_dir.close()
+# import os 
+# import tarfile
+# input_path = '/home/harshitadd/gu_darts/'
+# years = os.listdir(input_path)
+# for year in years:
+#   year_dir = tarfile.open(year)
+#   year_dir.extractall(input_path)
+#   year_dir.close()
 
-  
-  
+import json
+import os 
+year = '2019' 
+months = os.listdir('/home/harshitadd/en_darts/' + year)
+for month in months:
+  days = os.listdir('/home/harshitadd/en_darts/' + year + '/' + month + '/')
+  for day in days:
+    articles = os.listdir('/home/harshitadd/en_darts/' + year + '/' + month + '/' + day + '/')
+    for article in articles:
+      sentences = []
+      input_path = '/home/harshitadd/en_darts/' + year + '/' + month + '/' + day + '/' + article 
+      with open(input_path) as f:
+        try: 
+          data = json.load(f)
+          sentences.append(data['sentences'])
+          file_input_name = '/home/harshitadd/' 
+          with open(file_input_name,'w') as file:
+            file.write('\n'.join(sentences)) 
+        except:
+          print('Failed for ' +  year + '/' + month + '/' + day + '/' + article)
